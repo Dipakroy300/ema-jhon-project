0d7bc3d4a561c2380e6a0b47cabe366c6bfe4387
Dipakroy300 <dipakroy300@gmail.com>
2024-02-16 16:20:23 +0530

    btn added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,11 +4,17 @@ import './Product.css';
 const Product = (props) => {
     // console.log(props.product);
 
-    const {img,name,seller,quantity,price}=props.product;
+    const { img, name, seller, quantity, ratings, price } = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h6>{name}</h6>
+            <div className='product-info'> 
+                <h6 className='product-name'>{name}</h6>
+                <p>Price:${price}</p>
+                <p>Manufacturer:{seller}</p>
+                <p>Rating:{ratings} stars</p>
+            </div>
+            <button className='btn-cart'>Add To Cart</button>
         </div>
     );
 };
